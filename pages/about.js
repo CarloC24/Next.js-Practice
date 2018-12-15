38eae04b23a9e44ec8e7dd7726779cbb8899cc3f
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-12-14 18:17:44 -0600

    added index and about

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Navbar from '../components/NavBar';
+
+const About = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>About BitzPrice</h1>
+      <p>Application to view BitCoin Prices</p>
+    </div>
+  );
+};
+
+export default About;
