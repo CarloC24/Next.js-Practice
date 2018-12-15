e34701aafa407546e8367dc58413483c8144db8c
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-12-15 15:55:12 -0600

    added nprogress and refactored to have a custom app component

---
+++ components/Head.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+const Meta = () => {
+  return (
+    <Head>
+      <title>BitzPrice</title>
+      <link
+        rel="stylesheet"
+        href="https://bootswatch.com/4/cerulean/bootstrap.min.css"
+      />
+      <link rel="stylesheet" type="text/css" href="../nprogress.css" />
+    </Head>
+  );
+};
+
+export default Meta;
--- components/_app.js
+++ components/_app.js
@@ -1,1 +1,43 @@
-import App, { Component } from 'next/app';
+import App, { Container } from 'next/app';
+import Router from 'next/router';
+import { ThemeProvider } from 'styled-components';
+import Head from './Head';
+import Navbar from './NavBar';
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0,0,0,0.09)'
+};
+
+class myApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    //this exposes the query to the user
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Container>
+        <ThemeProvider theme={theme}>
+          <Head />
+          <Navbar />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Container>
+    );
+  }
+}
+
+export default MyApp;
